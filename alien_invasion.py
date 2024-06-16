a9ded5e671c945bc359db0a6cef248301f2407aa
joshcordek <josh_cordek1@baylor.edu>
2024-06-16 14:44:05 -0700

    Update alien_invasion.py
    Deletes old bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,12 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            
+            #Get rid of bullets that have disappeared
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
             self._update_screen()
             
             #Redraw the screen during each pass through the loop.
