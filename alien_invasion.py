ab1336912fa09dae2e5a5df1f8c800a4c1c5febf
joshcordek <josh_cordek1@baylor.edu>
2024-06-16 14:07:22 -0700

    Update alien_invasion.py
    Added quit option and fullscreen display

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_with, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -49,6 +51,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
                 
     def _check_keyup_events(self, event):            
         if event.key == pygame.K_RIGHT:
@@ -57,6 +61,7 @@ class AlienInvasion:
             self.ship.moving_left = False
             # Move the ship to the right.
             self.ship.rect.x += 1
+    
     def _update_screen(self):
         """Update images on screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
