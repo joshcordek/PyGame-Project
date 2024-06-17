847e676e68b44d65f86c3d556d9d9a3df06f6bfc
joshcordek <josh_cordek1@baylor.edu>
2024-06-17 08:58:45 -0700

    Update alien_invasion.py
    Drops alien fleet and detects if aliens get hit by a bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -52,6 +52,10 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
         print(len(self.bullets))
         self._update_screen()
+
+        #Check for any bullets that have hit aliens
+        #If so, get rid of the bullet and the alien
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
             
         #Redraw the screen during each pass through the loop.
         self.screen.fill(self.bg_color)
@@ -132,9 +136,28 @@ class AlienInvasion:
         self.aliens.add(alien)
 
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet"""
+        """Check if the fleet is at an edge, then update the positions of all alines in the fleet"""
+        self._check_fleet_edges()
         self.aliens.update()
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
+if __name__ == 'main':
+    #Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
 
 if __name__ == 'main':
     #Make a game instance, and run the game.
