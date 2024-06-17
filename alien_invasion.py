09e9cd3d8d8dca47b44b5c7b170874d84d5614ae
joshcordek <josh_cordek1@baylor.edu>
2024-06-17 11:56:47 -0700

    Update alien_invasion.py
    Adds the scoreboard to the main alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,6 +88,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self._create.increase_speed()
+
+            #Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
         
         print(len(self.bullets))
         self._update_screen()
@@ -121,6 +125,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             #Get rid of any remaining aliens and bullets
             self.aliens.empty()
